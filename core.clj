d6215c9fe1828d3998ac02ba0f70758e4c2f49f1
kruzabasi <kruzabasi@gmail.com>
2021-09-02 09:10:58 +0100

    a better way to do it

--- core.clj
+++ core.clj
@@ -7,4 +7,15 @@
       (recur (if (even? n) (/ n 2) (+ (* n 3) 1)))))
 
 (colatz-fn 99)
-; returns 99 298 149 448 224 112 56 28 14 7 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1 => "done here"
+; returns => 99 298 149 448 224 112 56 28 14 7 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1 => "done here"
+
+(defn collatz-con [n]
+  (let [next-pos    (/ (last n) 2)
+        next-neg    (+ (* (last n) 3) 1)]
+    (if (<= (last n) 1) n
+        (recur (cond (even? (last n)) (conj n next-pos)
+                     :else (conj n next-neg))))))
+
+(collatz-con [369])
+; returns => [369 1108 554 277 832 416 208 104 52 26 13 40 20 10 5 16 8 4 2 1]
+
